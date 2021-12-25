0086701594588c1d04a77887965e8dcffd2bd988
Jeff Meuris <jeff.meuris@gmail.com>
2021-12-25 23:57:31 +0100

    start work on wpf project

---
+++ Herhaling_Pakjesdienst.Models/PackageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herhaling_Pakjesdienst.Models
+{
+    // this is an enum, basically a custom type of which you get to decide what values it can be
+    // I could just as easily use a bool or a string over in MainWindow.xaml.cs, but this is neater
+    // more info: https://docs.microsoft.com/en-us/dotnet/api/system.enum?view=netcore-3.1
+    public enum PackageType
+    {
+        OvernightPackage,
+        TwodayPackage
+    }
+}
--- Herhaling_Pakjesdienst.WPF/MainWindow.xaml.cs
+++ Herhaling_Pakjesdienst.WPF/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+// using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Herhaling_Pakjesdienst.Models;
 
 namespace Herhaling_Pakjesdienst.WPF
 {
@@ -20,9 +21,71 @@ namespace Herhaling_Pakjesdienst.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        //=private=variables=======================================================================
+        // PackageType is an enum, more info in PackageType.cs over in the Models project
+        // the ? makes this a nullable type, meaning it can be whatever I said the enum can be (in PackageType.cs) OR null!
+        // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/nullable-value-types
+        private PackageType? _packageType;
+
+        // constructor
+        // this gets called when the program starts so I use it instead of window loaded event
         public MainWindow()
         {
             InitializeComponent();
+            InitLogic();
+        }
+
+        //=private=methods=========================================================================
+        // these don't get called by the MainWindow controls
+        private void InitLogic()
+        {
+            // this method gets called in the constructor and handles stuff that
+            // needs to be done when the program starts like disabling the update button
+            // and loading in the data from the text file
+            // could just as easily place all this code in the constructor, but this looks neater
+        }
+
+        private void UpdateLabel()
+        {
+            if (_packageType.HasValue) // test if _packageType is not null (because it's defined as a nullable type higher up)
+            {
+                // this is a neat shorter way to write an if statement
+                // structure: [thing that will get changed] = [some condition] ? [value when condition is met] : [value when condition is not met];
+                // more info: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/conditional-operator
+                lblSoortZending.Content = _packageType.Value == PackageType.OvernightPackage ?
+                    "Toeslag per Kg" : "Vaste Kost";
+            }
+        }
+
+        //=event=handlers==========================================================================
+        // these get called when the user interacts with certain controls the MainWindow
+        private void SoortVerzending_Click(object sender, RoutedEventArgs e)
+        {
+            // this event gets fired when the radiobuttons that decide the type of the package get checked
+            // both radiobuttons trigger the same event.
+            // Because they're in the same groupbox, only one can be selected at any given time
+
+            // the button that fired the event is passed along as the sender object
+            // to use it, we need to cast it back to its original class
+            RadioButton btn = sender as RadioButton;
+
+            // decide what to do based on the name of the button that fired the event
+            switch (btn.Name)
+            {
+                // sets _packageType to whatever package type was selected
+                // this way other methods can at all times check which type of package is selected
+                // without having to go and check which radiobutton is selected every time, who has time for that, right?
+                // the names are set in the MainWindow.xaml file
+
+                case "rdbTwoday":
+                    _packageType = PackageType.TwodayPackage;
+                    break;
+                case "rdbOvernight":
+                    _packageType = PackageType.OvernightPackage;
+                    break;
+            }
+
+            UpdateLabel();
         }
     }
 }
